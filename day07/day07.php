569b889b7532c74d481e8bf46d52333e653788db
Ronald Großmann <dev@ronaldg.net>
2021-12-07 17:49:39 +0100

    rms test
    tested if Root Mean Square is a non bruteforce attempt for part 2
    ... turns out its not

--- day07/day07.php
+++ day07/day07.php
@@ -9,28 +9,34 @@ $example = explode(",", trim($example));
 $positions = $input;
 $positions_sorted = $input;
 sort($positions_sorted, SORT_NUMERIC);
+$size = sizeof($positions);
 
-$average = round(array_sum($positions)/sizeof($positions));
+$average = round(array_sum($positions)/$size);
 $median = $positions_sorted[abs(sizeof($positions_sorted)/2)];
+$rms = round(sqrt(array_sum(array_map(function($val){return $val**2;},$positions))/$size));
+
 $min = $positions_sorted[0];
 $max = $positions_sorted[sizeof($positions_sorted)-1];
 
 $fcAverage = fuelConsumption($positions, $average);
 $fcMedian = fuelConsumption($positions, $median);
+$fcRms = fuelConsumption($positions, $rms);
 $fcBruteforce = bruteforce('fuelConsumption', $positions, $min, $max);
 
 $cfcAverage = crabFuelConsumption($positions, $average);
 $cfcMedian = crabFuelConsumption($positions, $median);
+$cfcRms = crabFuelConsumption($positions, $rms);
 $cfcBruteforce = bruteforce('crabFuelConsumption', $positions, $min, $max);
 
 echo "Average: ".$average." Fuel: ".$fcAverage." Crabfuel: ".$cfcAverage."\n";
 echo "Median: ".$median." Fuel: ".$fcMedian." Crabfuel: ".$cfcMedian."\n";
+echo "Root Mean Square: ".$rms." Fuel: ".$fcRms." Crabfuel: ".$cfcRms."\n";
 echo "Bruteforce: ".$fcBruteforce[1]."|".$cfcBruteforce[1]." Fuel: ".$fcBruteforce[0]." Crabfuel: ".$cfcBruteforce[0]."\n";
 
 function fuelConsumption(Array $positions, int $p) : int 
 {
     $distances = array_map(function($position)use($p){
-       return abs($position-$p); 
+        return abs($position-$p); 
     }, $positions);
     
     return array_sum($distances);
